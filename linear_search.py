4a273083fcfe87b867442ea6c7f9c5fbd62fdb65
Bharathi Kannan <112495342+bharathi0811@users.noreply.github.com>
2023-07-12 16:27:08 +0530

    linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,9 @@
 arr=list(map(int,input().split()))
 b= int(input("Enter the value:"))
-count=0
-for i in arr:
-    if i ==b:
-        count+=1
-print(count)
+def count_num(arr,b):
+    count=0
+    for i in arr:
+        if i ==b:
+            count+=1
+    return count
+print(count_num(arr,b)
